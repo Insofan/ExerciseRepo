29bf27401938cfa68a691e8baf4d55acd314ec9b
Insomnia <282132315@qq.com>
2019-02-26 21:56:17 +0800

    sync

---
+++ 021901/Tree0226.cpp
@@ -0,0 +1,106 @@
+// 
+// @ClassName Tree0226
+// @Description TODO
+// @Date 19-2-26 下午9:14
+// @Created by Insomnia
+//
+
+#include <iostream>
+#include <stack>
+
+struct BSTree{
+    int val;
+    BSTree *left;
+    BSTree *right;
+
+    BSTree(int x): val(x), left(NULL), right(NULL){};
+};
+
+
+using namespace std;
+class Solution {
+public:
+    void preOrder(BSTree *root) {
+
+        if (!root) {
+            return;
+        }
+
+        stack<BSTree *> s;
+
+        BSTree *p = root;
+        while (!s.empty() || p) {
+            while (p) {
+               cout << p->val << " ";
+               s.push(p);
+               p = p->left;
+            }
+
+            p = s.top();
+            s.pop();
+            p = p->right;
+        }
+    }
+
+    void inOrder(BSTree *root) {
+
+        if (!root) {
+            return;
+        }
+
+        stack<BSTree *> s;
+
+        BSTree *p = root;
+        while (!s.empty() || p) {
+            while (p) {
+               s.push(p);
+               p = p->left;
+            }
+
+            p = s.top();
+            cout << p->val << " ";
+            s.pop();
+            p = p->right;
+        }
+    }
+
+    void postOrder(BSTree *root) {
+        if (!root) {
+            return;
+        }
+
+        stack<BSTree *> s;
+        BSTree *p = root;
+        BSTree *last = NULL;
+
+        while (!s.empty() || p) {
+            if (p->left) {
+                s.push(p);
+                p = p->left;
+            } else {
+                BSTree *tmp = s.top();
+                if
+            }
+        }
+    }
+};
+
+int main() {
+    BSTree a1(10);
+    BSTree a2(5);
+    BSTree a3(12);
+    BSTree a4(4);
+    BSTree a5(7);
+
+    a1.left = &a2;
+    a1.right = &a3;
+
+    a2.left = &a4;
+    a2.right = &a5;
+
+    Solution sol;
+
+    sol.inOrder(&a1);
+
+    return 0;
+}
---
+++ 0225/TreeOrder0206.cpp
@@ -0,0 +1,6 @@
+// 
+// @ClassName TreeOrder0206
+// @Description TODO
+// @Date 19-2-26 下午9:13
+// @Created by Insomnia
+//
